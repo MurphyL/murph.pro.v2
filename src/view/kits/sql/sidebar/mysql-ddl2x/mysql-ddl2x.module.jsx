9fe6d0a61fcec8a8becf518cf0af642a04149795
hao.luo <hao.luo@china.zhaogang.com>
2022-09-01 13:18:08 +0800

    DataX 配置

--- src/view/kits/sql/sidebar/mysql-ddl2x/mysql-ddl2x.module.jsx
+++ src/view/kits/sql/sidebar/mysql-ddl2x/mysql-ddl2x.module.jsx
@@ -1,7 +1,7 @@
 import { SQLEditor } from "../../layout/sql-kits-layout.v1.module";
 import Splitter from "/src/plug/widgets/container/splitter/splitter.v1.module";
 
-export default function MySQL_DDL2X_Layout() {
+export default function MySQL_DDL2X() {
     return (
         <Splitter>
             <SQLEditor />
