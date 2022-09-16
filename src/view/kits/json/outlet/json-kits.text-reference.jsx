1daf575627c76b7a258fbf758b12fabd6be6c976
hao.luo <hao.luo@china.zhaogang.com>
2022-09-16 13:29:12 +0800

    JSONKit 导入 CSV 文件

--- src/view/kits/json/outlet/json-kits.text-reference.jsx
+++ src/view/kits/json/outlet/json-kits.text-reference.jsx
@@ -25,13 +25,17 @@ export default React.memo(function JSONKitsTextReference() {
             });
         }
     }, [getEditorContent, setEditorContent, setEditorLanguage]);
-    const loadCSVFile = React.useCallback(async ([ file ]) => { 
-        fromCSVFile(file).then((payload) => {
-            setEditorContent(format(payload));
-            console.log('导入 CSV 文件', file.name, '成功');
-        }).catch(e => {
-            console.error('导入 CSV 文件', file.name, '失败');
-        });
+    const loadFile = React.useCallback(async ([ file ], fileType) => { 
+        switch(fileType) {
+            case 'csv':
+                fromCSVFile(file).then((payload) => {
+                    setEditorContent(format(payload));
+                    console.log('导入 CSV 文件', file.name, '成功');
+                }).catch(e => {
+                    console.error('导入 CSV 文件', file.name, '失败', e);
+                });
+                break;
+        }
     }, [setEditorContent]);
     return (
         <React.Fragment>
@@ -39,14 +43,14 @@ export default React.memo(function JSONKitsTextReference() {
             <Button variant="outlined" onClick={() => doConvent((source) => format(source, false))}>压缩</Button>
             <Button variant="outlined" onClick={() => pushState('/kits/json/tree-view')}>Tree View</Button>
             <br />
+            <Button variant="outlined" onClick={() => pushState('/kits/json/path-query')}>发送到 JSONPath Query</Button>
+            <br />
             <Button variant="outlined" component="label">
-                <input hidden={true} accept="*" type="file" onChange={e => loadCSVFile(e.target.files)} />
+                <input hidden={true} accept="*" type="file" onChange={e => loadFile(e.target.files, 'csv')} />
                 <span>导入 CSV 文件</span>
             </Button>
             {/* TODO - 导出 CSV 文件 */}
             {/* <Button variant="outlined">导出 CSV 文件</Button> */}
-            <br />
-            <Button variant="outlined" onClick={() => pushState('/kits/json/path-query')}>发送到 JSONPath Query</Button>
             <br />
             <Button variant="outlined" onClick={() => doConvent(toYAML, 'yaml')}>转换为 YAML</Button>
             <Button variant="outlined" onClick={() => doConvent(toXML, 'xml')}>转换为 XML</Button>
