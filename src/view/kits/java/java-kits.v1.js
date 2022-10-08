1f1ea676c5cd907ada31b720513d47fc21ae9941
murph <murphyl@outlook.com>
2022-10-08 22:37:25 +0800

    DDL 工具完善抽象

--- src/view/kits/java/java-kits.v1.js
+++ src/view/kits/java/java-kits.v1.js
@@ -1,13 +1,35 @@
 import { join } from "lodash";
 
-const createClass = (name) => {
+const indent = (content) => {
+    return `    ${content}`;
+};
+
+const createClass = (name, fields = []) => {
+    const prepared = [];
+    if (Array.isArray(fields) && fields.length > 0) {
+        fields.forEach(field => {
+            prepared.push(indent('/**'));
+            field.comment && prepared.push(indent(` * ${field.comment}`));
+            if (field.database) {
+                const { column, length, defaultValue, notNull } = field.database;
+                column && prepared.push(indent(` * database column: ${column}`));
+                defaultValue && prepared.push(indent(` * database default: ${defaultValue}`));
+                length && prepared.push(indent(` * database length: ${length}`));
+                notNull && prepared.push(indent(' * database not allow null'));
+            }
+            prepared.push(indent(' */'));
+            prepared.push(indent(`private ${field.dataType} ${field.name};\n`))
+        });
+    }
     return [
-        `public class ${name} {}`
+        `public class ${name} {\n`,
+        ...prepared,
+        '}'
     ];
 };
 
-export const createPojoClass = ({ name }) => {
-    return join(createClass(name), '\n');
+export const createPojoClass = ({ comment, name, fields }) => {
+    return join([`/** ${comment} **/`, ...createClass(name, fields)], '\n');
 };
 
 export const createMapperClass = () => {
--- src/view/kits/sql/ddl-to-x.jsx
+++ src/view/kits/sql/ddl-to-x.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { useRecoilState } from 'recoil';
+import { useSetRecoilState } from 'recoil';
 
 import { select, insert } from 'sql-bricks';
 import { camelCase, trim, upperFirst, zipObject } from 'lodash';
@@ -13,7 +13,7 @@ import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
 import SvgIcon from '@mui/material/SvgIcon';
 
-import { SiMysql } from "react-icons/si";
+import { SiJava, SiMysql } from "react-icons/si";
 import { SiAlibabacloud } from "react-icons/si";
 
 
@@ -34,25 +34,54 @@ import { sqlEditorState, format as formatSQL } from '../sql/sql-kits.v1';
 import { createPojoClass } from '../java/java-kits.v1';
 
 const rendersV1 = {
-    'java-classes': {
-        label: 'DataX Options',
-        icon: (<SvgIcon><SiAlibabacloud /></SvgIcon>),
-        exec: (schema, source) => {
-            return [true, {
-                language: 'java',
-                content: 'xx'
-            }];
-        }
+    'java/classes': {
+        label: 'Java POJO Class',
+        icon: (<SvgIcon><SiJava /></SvgIcon>),
     },
-    'sqls-for-table': {
+    'datax/options': {
+        label: 'Java POJO Class',
+        icon: (<SvgIcon><SiAlibabacloud /></SvgIcon>),
+    },
+    'common/sqls': {
         label: 'Data Query Language',
         icon: (<SvgIcon><SiMysql /></SvgIcon>),
-        exec: (schema) => {
-            return [true, {
+    }
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'message':
+            return {
+                message: action.message || '暂无可渲染数据',
+                level: action.level
+            };
+        case 'ddl/parsed':
+            return {
+                language: 'json', parsed: action.parsed, content: JSON.stringify(action.parsed, null, 4)
+            };
+        case 'java/classes':
+            return {
+                language: 'java',
+                action: action.type,
+                parsed: state.parsed,
+                content: convertParsedDDL2POJO(state.parsed)
+            };
+        case 'common/sqls':
+            return {
                 language: 'sql',
-                content: convertParsedDDL2SQLS(schema)
-            }];
-        }
+                action: action.type,
+                parsed: state.parsed,
+                content: convertParsedDDL2SQLs(state.parsed)
+            };
+        case 'datax/options':
+            return {
+                language: 'json',
+                action: action.type,
+                parsed: state.parsed,
+                content: convertParsedDDL2DataXOptions(state.parsed)
+            };
+        default:
+            return state;
     }
 };
 
@@ -60,16 +89,16 @@ export default function DDL2X() {
     useDocumentTitle('DDL 工具集');
     const editorRef = React.useRef(null);
     const { enqueueSnackbar } = useSnackbar();
-    const [action, setAction] = React.useState(null);
-    const [schema, setSchema] = React.useState(null);
-    const [source, setSqlEditorState] = useRecoilState(sqlEditorState);
+    const setSqlEditorState = useSetRecoilState(sqlEditorState);
+    const [state, dispatch] = React.useReducer(reducer, { message: '暂无可渲染数据' });
     const doParse = () => {
-        setSchema(null);
+        dispatch({ type: 'message', message: '正在解析 DDL' })
         if (!editorRef || !editorRef.current) {
             return;
         }
         const sql = trim(editorRef.current.getValue());
         if (sql.length === 0) {
+            dispatch({ type: 'message', message: '请输入一条 SQL' })
             enqueueSnackbar('请输入一条 SQL', {
                 autoHideDuration: 3000,
                 variant: 'error',
@@ -79,37 +108,31 @@ export default function DDL2X() {
         KITS_AXIOS_INSTANCE.post('/sql/ddl/parse', { sql }).then(resp => {
             const [success, content] = resolveServerKitResponse(resp);
             if (success) {
-                setAction('parsed-ddl-schema');
                 setSqlEditorState(sql);
-                setSchema(content);
+                dispatch({ type: 'ddl/parsed', parsed: content })
             } else {
+                dispatch({ type: 'message', message: content || '服务端错误' })
                 enqueueSnackbar(content || '服务端错误', {
                     autoHideDuration: 3000,
                     variant: 'error',
                 });
             }
         }).catch(e => {
+            dispatch({ type: 'message', message: e.message || '请求出错' })
             enqueueSnackbar(e.message || '请求出错', {
                 autoHideDuration: 3000,
                 variant: 'error',
             });
         })
     };
-    const [success, payload] = React.useMemo(() => {
-        if (action && rendersV1[action] && rendersV1[action].exec) {
-            return rendersV1[action].exec(schema, source);
-        } else {
-            return [false, '解析器不存在'];
-        }
-    }, [schema, action]);
     return (
         <Splitter sizes={[45, 55]} minSizes={[500, 300]}>
             <CodeEditor ref={editorRef} language="sql" />
             <Stack spacing={1} sx={{ margin: 1, p: 1 }}>
-                {success ? (
-                    <CodeBlock language={payload.language} children={payload.content} />
+                {state.message ? (
+                    <Alert severity="info" sx={{ p: 1.2 }}>{state.message}</Alert>
                 ) : (
-                    <Alert severity="info">{payload}</Alert>
+                    <CodeBlock language={state.language} children={state.content} />
                 )}
                 <Stack spacing={2} sx={{ position: 'absolute', top: 7, right: 16 }}>
                     <IconButton onClick={doParse}>
@@ -117,8 +140,8 @@ export default function DDL2X() {
                             <PodcastsIcon />
                         </Avatar>
                     </IconButton>
-                    {schema ? (
-                        <ToggleButtonGroup orientation="vertical" size="small" sx={{ bgcolor: '#fff' }} onChange={(e, [action]) => setAction(action)}>
+                    {state.parsed ? (
+                        <ToggleButtonGroup orientation="vertical" size="small" sx={{ bgcolor: '#fff' }} onChange={(e, [type]) => dispatch({ type })}>
                             {Object.entries(rendersV1).map(([key, render]) => (
                                 <ToggleButton key={key} value={key} aria-label={render.label}>
                                     {render.icon}
@@ -131,9 +154,6 @@ export default function DDL2X() {
         </Splitter >
     );
 }
-
-const EMPTY_LINE = '\n';
-const INDENT_STR = '   ';
 
 const SCHEMA_TYPES_MAP2JAVA = {
     VARCHAR: 'String',
@@ -148,75 +168,43 @@ const SCHEMA_TYPES_MAP2JAVA = {
     TIMESTAMP: 'Date',
 };
 
-const column2field = (column, index) => {
-    const fieldType = SCHEMA_TYPES_MAP2JAVA[column.dataType] || column.dataType;
-    const fieldName = camelCase(column.name) || `field${index}`;
-    const comments = [column.name];
-    column.comment && comments.push(column.comment);
-    column.constraint && comments.push(column.constraint);
-    const fieldParts = [fieldType, fieldName];
-    if (column.default && column.default !== 'NULL') {
-        fieldParts.push(`=`);
-        if (fieldType === 'String') {
-            fieldParts.push(`"${column.default}"`)
-        } else {
-            fieldParts.push(column.default);
-        }
+const createTableUnique = (schema, table) => {
+    const result = [table];
+    if (schema) {
+        result.unshift(schema);
     }
-    return [
-        INDENT_STR + `/** ${comments.join(' - ')} **/`,
-        INDENT_STR + `private ${fieldParts.join(' ')};` + EMPTY_LINE
-    ].join(EMPTY_LINE);
-}
-
-
-export const ddlKitsReducer = (state, action) => {
-    switch (action.type) {
-        case 'parsed-ddl-schema':
-            return {
-                language: 'json',
-                origin: action.type,
-                schema: action.content,
-                content: JSON.stringify(action.content, null, 4)
-            };
-        case 'sqls-for-table':
-            if (!state.schema) {
-                return { ...state, error: '未发现解析过的 DDL' };
-            } else {
-                return {
-                    language: 'sql',
-                    origin: action.type,
-                    schema: state.schema,
-                    content: convertParsedDDL2SQLS(state.schema)
-                };
-            }
-
-        case 'java-classes':
-            console.log(state.schema);
-            return {
-                language: 'java',
-                origin: action.type,
-                schema: state.schema,
-                content: convertParsedDDL2POJO(state.schema)
-            };
-        default:
-            return state;
-    }
+    return result.join('.');
 };
 
-const convertParsedDDL2SQLS = (schemaOfDDL = {}) => {
+const convertParsedDDL2SQLs = (schemaOfDDL) => {
     const tableColumns = schemaOfDDL.columns.map(({ name }) => name);
-    const tableUnique = [schemaOfDDL.schema, schemaOfDDL.table].join('.');
+    const tableUnique = createTableUnique(schemaOfDDL.schema, schemaOfDDL.table);
     return formatSQL([
         select(tableColumns).from(tableUnique).toString(),
         insert(tableUnique).values(zipObject(tableColumns, tableColumns.map(col => `#{${col}}`))).toString()
     ].join(';'));
 };
 
-const convertParsedDDL2POJO = (schemaOfDDL = {}) => {
-    console.log(schemaOfDDL);
-    const comment = `table: ${[schemaOfDDL.schema, schemaOfDDL.table].join('.')}`;
+const convertParsedDDL2POJO = (schemaOfDDL) => {
+    const tableUnique = createTableUnique(schemaOfDDL.schema, schemaOfDDL.table);
+    const comment = `database table: ${tableUnique}`;
+    const className = upperFirst(camelCase(schemaOfDDL.table));
+    const fields = schemaOfDDL.columns.map((column, index) => ({
+        index,
+        name: camelCase(column.name),
+        dataType: SCHEMA_TYPES_MAP2JAVA[column.dataType] || column.dataType,
+        database: {
+            column: column.name,
+            length: column.length,
+            defaultValue: column.default,
+            notNull: column.not_null,
+        },
+    }));
     return createPojoClass({
-        comment
+        name: className, comment, fields
     });
-}
+};
+
+const convertParsedDDL2DataXOptions = (schemaOfDDL) => {
+
+};
